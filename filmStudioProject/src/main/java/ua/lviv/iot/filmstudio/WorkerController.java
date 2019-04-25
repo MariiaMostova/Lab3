614b5975ee7eda1bbba11a02d4ffa3f7cfe74891
Mariia Mostova <were3437@gmail.com>
2019-04-25 22:57:55 +0300

    PUT method is edicted

--- filmStudioProject/src/main/java/ua/lviv/iot/filmstudio/WorkerController.java
+++ filmStudioProject/src/main/java/ua/lviv/iot/filmstudio/WorkerController.java
@@ -1,44 +1,58 @@
 package ua.lviv.iot.filmstudio;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import ua.lviv.iot.filmstudio.models.Worker;
 
 @RestController
 public class WorkerController {
- 
- @Autowired
- private WorkerRepository workerRepository;
 
- @RequestMapping(value = "/worker", method = RequestMethod.GET)
- public List<Worker> getWorkers() {
-  return (List<Worker>) workerRepository.findAll();
- }
- 
- @RequestMapping(value = "//{id}", method = RequestMethod.GET)
- public Worker getWorker(@PathVariable Integer id) {
-  return workerRepository.findById(id).get();
- }
+    @Autowired
+    private WorkerRepository workerRepository;
 
- @RequestMapping(value = "/worker", method = RequestMethod.POST)
- public void postWorker(@RequestBody Worker worker) {
-     workerRepository.save(worker);
- }
- 
- @RequestMapping(value = "/worker/{id}", method = RequestMethod.PUT)
- public void updateWorker(@RequestBody Worker worker, @PathVariable Integer id) {
-     workerRepository.save(worker);
- }
- 
- @RequestMapping(value = "/worker/{id}", method = RequestMethod.DELETE)
- public void deleteWorker(@PathVariable Integer id) {
-     workerRepository.deleteById(id);;
- }
+    @GetMapping(value = "/worker")
+    public List<Worker> getWorker() {
+        return (List<Worker>) workerRepository.findAll();
+    }
+
+    @GetMapping(value = "/worker/{id}")
+    public Worker getWorker(@PathVariable Integer id) {
+        return workerRepository.findById(id).get();
+    }
+
+    @PostMapping(value = "/worker")
+    public void postWorker(@RequestBody Worker worker) {
+        workerRepository.save(worker);
+    }
+
+    @PutMapping(value = "/worker/{id}")
+    public Worker updateWorker(@RequestBody Worker newWorker, @PathVariable Integer id) {
+        Optional<Worker> workerOne = workerRepository.findById(id);
+        Worker worker = workerOne.get();
+        worker.setFirstName(newWorker.getFirstName());
+        worker.setLastName(newWorker.getLastName());
+        worker.setProfession(newWorker.getProfession());
+        worker.setDeegre(newWorker.isDeegre());
+        worker.setCreatedDocumentalFilms(newWorker.getCreatedDocumentalFilms());
+        worker.setWishSalary(newWorker.getWishSalary());
+        worker.setWorkExperience(newWorker.getWorkExperience());
+        return workerRepository.save(newWorker);
+    }
+
+    @DeleteMapping(value = "/worker/{id}")
+    public void deleteWorker(@PathVariable Integer id) {
+        workerRepository.deleteById(id);
+        ;
+    }
 }
-
--- filmStudioProject/src/main/java/ua/lviv/iot/filmstudio/WorkerRepository.java
+++ filmStudioProject/src/main/java/ua/lviv/iot/filmstudio/WorkerRepository.java
@@ -1,9 +1,12 @@
 package ua.lviv.iot.filmstudio;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import ua.lviv.iot.filmstudio.models.Worker;
 
 public interface WorkerRepository 
     extends CrudRepository<Worker, Integer> {
 
+    Optional<Worker> findById(Long id);
 }
